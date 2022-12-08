6b11ec0830e48f673ff5498a44bbd1c511c8c8b7
Sophia DeVito <sophiadevito@Sophias-MacBook-Pro.local>
2022-12-07 22:38:00 -0500

    P2 Color Updates

--- SoundFur/SoundFur.ino
+++ SoundFur/SoundFur.ino
@@ -3,6 +3,20 @@ int buttonState1 = 0;  // variable for reading the pushbutton status
 int buttonState2 = 0;
 int buttonState3 = 0;
 int buttonState4 = 0;
+int movement1 = 0;
+int movement2 = 0;
+int movement3 = 0;
+int p1a = 0;
+int p1b = 0;
+int p1c = 0;
+int p2a = 0;
+int p2b = 0;
+int p2c = 0;
+int p3a = 0;
+int p3b = 0;
+int p3c = 0;
+
+unsigned char val = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -10,18 +24,74 @@ void setup() {
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
   pinMode(16, INPUT_PULLUP);
+
+  pinMode(11, OUTPUT);
+  //waitForSuperCollider();
 }
 
 void loop() {
   // read the state of the pushbutton value:
+
   buttonState1 = digitalRead(2);
   buttonState2 = digitalRead(4);
   buttonState3 = digitalRead(5);
   buttonState4 = digitalRead(16);
 
   int p1 = analogRead(33); //P1 sensor
+  delay(10); // delay in between analog reads for stability
   int p2 = analogRead(32); //P2 sensor
+  delay(10);
   int p3 = analogRead(35); //P3 sensor
+
+  //if one of the photoresistors has been triggered, movement is true
+  if (p1 != p1c)
+    movement1 = 1;
+  else 
+    movement1 = 0;
+  if (p2 != p2c)
+    movement2 = 1;
+  else 
+    movement2 = 0;
+  if (p3 != p3c)
+    movement3 = 1;
+  else 
+    movement3 = 0;
+
+  //store last 3 photoresistor numbers
+  p1c = p1b;
+  p1b = p1a;
+  p1a = p1;
+  p2c = p2b;
+  p2b = p2a;
+  p2a = p2;
+  p3c = p3b;
+  p3b = p3a;
+  p3a = p3;
+
+  /*
+  if (Serial.available() > 0) {
+    Serial.read();
+    Serial.print(movement1);
+    Serial.print(",");
+    Serial.print(p1);
+    Serial.print(",");
+    Serial.print(movement2);
+    Serial.print(",");
+    Serial.print(p2);
+    Serial.print(",");
+    Serial.print(movement3);
+    Serial.print(",");
+    Serial.print(p3);
+    Serial.print(",");
+    Serial.print(buttonState1);
+    Serial.print(",");
+    Serial.print(buttonState2);
+    Serial.print(",");
+    Serial.print(buttonState3);
+    Serial.print(",");
+    Serial.println(buttonState4); //end of data line
+  }
+  */
 
   Serial.println(p1);
   Serial.println(p2);
@@ -44,3 +114,12 @@ void loop() {
     Serial.println("FOUR");
   } 
 }
+
+void waitForSuperCollider() {
+  while (Serial.available() <= 0) {
+    // send an initial string. must match the number of values you want to send
+    Serial.println("0,0,0,0,0,0,0,0,0,0");
+    delay(300);
+  }
+}
+
